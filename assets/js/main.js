b4d2e62463107b9ab8f65927aca174310d278f20
RodrigoBergenthal <rodrigo_bergenthal@live.com>
2022-11-18 07:24:55 -0300

    atualizando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-console.log('sucesso!')
+
+
+
+
+const offset = 0
+const limite = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}0&limit=${limite}`
+
+fetch(url).then (function (response){
+    response.json().then(function(responseBody){
+        console.log(responseBody)
+    })
+}
+)
+.catch(function(){
+    console.error(error)
+})
+.finally(function(){
+    console.log ('Requisição concluida!')
+})
